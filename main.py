54a5031e3ee9a87e89e4b3a4d4b58b0245516eae
Mirkitow <mirkowesner03@gmail.com>
2026-06-12 18:49:51 -0300

    Update Main()

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import Funciones
 
-opcion = 0
+lst_identificadores = []
+lst_montos = []
+lst_distancias = []
+lst_estados = []
+lst_repartidores = []
+lst_tiempo_estimado = []
+lst_prioridades = []
 
-while opcion != 5:
-    opcion = Funciones.menu(1, 5)
-    if opcion == 1:
-        Funciones.registrar_nuevo_pedido()
-    elif opcion == 2:
-        Funciones.eliminar_pedido()
-    elif opcion == 3:
-        Funciones.modificar_estado_o_repartidor()
-    elif opcion == 4:
-        Funciones.informe_general()
-    elif opcion == 5:
-        print("Saliendo del programa...")
+def main():
+    opcion = 0
     
+    while opcion != 5:
+        opcion = Funciones.menu(1, 5)
+        if opcion == 1:
+            Funciones.registrar_nuevo_pedido()
+        elif opcion == 2:
+            Funciones.eliminar_pedido()
+        elif opcion == 3:
+            Funciones.modificar_estado_o_repartidor()
+        elif opcion == 4:
+            Funciones.informe_general()
+        elif opcion == 5:
+            print("Saliendo del programa...")
+    
+main()
